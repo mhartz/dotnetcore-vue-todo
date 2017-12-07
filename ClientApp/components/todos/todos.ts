c8d71442c279b71444fa0e552f694a73d3f6d593
mhartz <mshartz5@gmail.com>
2017-12-06 23:52:44 -0500

    Todo started

---
+++ ClientApp/components/todos/todos.ts
@@ -0,0 +1,21 @@
+﻿import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+
+interface TodoItem {
+    description: string;
+    done: boolean;
+    id: string;
+}
+
+@Component
+export default class TodoComponent extends Vue {
+    todos: TodoItem[];
+    newItemDescription: string;
+
+    data() {
+        return {
+            todos: [],
+            newItemDescription: null
+        };
+    }
+}
